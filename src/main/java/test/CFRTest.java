84581f30eb0094168c96b8817e6c5aa0a4dd2267
Shane Bishop <shane.bishop@outlook.com>
2020-03-02 13:27:02 -0500

    Switched to Maven project directory structure

---
+++ src/main/java/test/CFRTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CFRTest {
+
+    @Test
+    public void testTemp() {
+        assertTrue(true);
+    }
+
+//    @Test
+//    public void
+
+}
